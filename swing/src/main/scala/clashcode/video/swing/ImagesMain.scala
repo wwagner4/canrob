fff97e8f889fa845d005848137bc53bb3316dfbc
wwagner4 <wwagner4@gmail.com>
2014-03-06 23:24:02 +0100

    refactored images main

--- swing/src/main/scala/clashcode/video/swing/ImagesMain.scala
+++ swing/src/main/scala/clashcode/video/swing/ImagesMain.scala
@@ -1,19 +1,25 @@
 package clashcode.video.swing
 
-import java.awt.{Graphics2D, RenderingHints}
+import java.awt.{ Graphics2D, RenderingHints }
 import java.awt.image.BufferedImage
 import java.io.File
-import clashcode.video.{DrawArea, ImageProvider_V01, NumberedStage, Rec, StageParams}
+import clashcode.video.{ DrawArea, ImageProvider_V01, NumberedStage, Rec, StageParams }
 import javax.imageio.ImageIO
 import doctus.core._
 import doctus.swing._
 import clashcode.video.Pos
+import clashcode.video.lists.AkkaWorkshopResultsVideos
+import clashcode.video.VideoCreator
 
 /**
  * Creates images in a directory that can be used to create videos afterward
  * by using appropriate tools.
  */
-class ImagesDevice  {
+object ImagesMain extends App {
+
+  val video = AkkaWorkshopResultsVideos.v001
+  val stages = VideoCreator.create(List(video), 20)
+  val stageParams = StageParams(10, ImageProvider_V01, 0.6, 0.03)
 
   //val res = Rec(3840, 2160) // 2160p
   //val res = Rec(2560, 1440)
@@ -21,9 +27,8 @@ class ImagesDevice  {
   //val res = Rec(1600, 900)
   val res = Rec(640, 360)
   val imgFormat = "png" // jpg, png
-    
-  val stageParams = StageParams(10, ImageProvider_V01, 0.6, 0.03)
-
+  
+  stages.foreach(s => paintStage(s))
   def paintStage(stage: NumberedStage): Unit = {
     val bi = new BufferedImage(res.w, res.h, BufferedImage.TYPE_INT_RGB)
     val da = DrawArea(Pos(0, 0), Rec(res.w, res.h))
@@ -36,7 +41,6 @@ class ImagesDevice  {
     val outDir = new File(home, "video")
     val mkdirsOK = if (outDir.exists()) true else outDir.mkdirs()
     if (mkdirsOK) {
-
       val nr: String = "%05d" format stage.nr
       val fileName = s"img$nr.$imgFormat"
       val file = new File(outDir, fileName)
