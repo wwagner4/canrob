5b9a7fcd169b63a25c89ab6d784e1915acf807cf
wwan <wwagner4@gmail.com>
2014-03-06 12:51:10 +0100

    formatting

--- project/CanrobBuild.scala
+++ project/CanrobBuild.scala
@@ -9,71 +9,70 @@ import scala.scalajs.sbtplugin._
 import ScalaJSPlugin._
 import ScalaJSKeys._
 
-
 object CanrobBuild extends Build {
 
-    object D {
-        val scalaVersion = "2.10.3"
-    }
+  object D {
+    val scalaVersion = "2.10.3"
+  }
 
-    object S {
+  object S {
 
-        lazy val baseSettings = 
-             Defaults.defaultSettings ++
-             Seq(
-               organization := "net.entelijan",
-               organizationHomepage := Some(url("http://entelijan.net/")),
-               libraryDependencies += "org.scalatest" %% "scalatest" % "2.0" % "test",
-               EclipseKeys.createSrc := EclipseCreateSrc.Default + EclipseCreateSrc.Resource,
-               EclipseKeys.withSource := true)
-				
-        lazy val defaultSettings = 
-             baseSettings ++
-			       Seq(
-			         version := "1.0-SNAPSHOT")
-			         
-			  lazy val coreSettings = 
-			       defaultSettings ++
-			       scalaJSSettings 
+    lazy val baseSettings =
+      Defaults.defaultSettings ++
+        Seq(
+          organization := "net.entelijan",
+          organizationHomepage := Some(url("http://entelijan.net/")),
+          libraryDependencies += "org.scalatest" %% "scalatest" % "2.0" % "test",
+          EclipseKeys.createSrc := EclipseCreateSrc.Default + EclipseCreateSrc.Resource,
+          EclipseKeys.withSource := true)
 
-			  lazy val swingSettings = 
-			       defaultSettings ++
-			       Seq(
-			          libraryDependencies += "org.scala-lang" % "scala-swing" % D.scalaVersion,
-			          fork := true)
-			          
-			  lazy val scalajsSettings = 
-			       defaultSettings ++ 
-			       scalaJSSettings ++
-			       Seq(
-                libraryDependencies += "org.scala-lang.modules.scalajs" %% "scalajs-dom" % "0.2",
-                libraryDependencies += "org.scala-lang.modules.scalajs" %% "scalajs-jquery" % "0.2",
-			          fork := true,
-			          unmanagedSources in (Compile, packageJS) += baseDirectory.value / "js" / "startup.js")			          
-			          
-			          
+    lazy val defaultSettings =
+      baseSettings ++
+        Seq(
+          version := "1.0-SNAPSHOT")
 
-	  }
+    lazy val coreSettings =
+      defaultSettings ++
+        scalaJSSettings
 
+    lazy val swingSettings =
+      defaultSettings ++
+        Seq(
+          libraryDependencies += "org.scala-lang" % "scala-swing" % D.scalaVersion,
+          fork := true)
 
-    lazy val root = Project(
-                          id = "canrob-root",
-                          base = file("."),
-							            settings = S.defaultSettings)
-							            .aggregate(core, swing, scalajs)
-    lazy val core = Project(
-                          id = "canrob-core",
-							            base = file("core"),
-							            settings = S.coreSettings)
-    lazy val swing = Project(
-                          id = "canrob-swing",
-							            base = file("swing"),
-							            settings = S.swingSettings)
-							            .dependsOn(core)
-    lazy val scalajs = Project(
-                          id = "canrob-scalajs",
-							            base = file("scalajs"),
-							            settings = S.scalajsSettings)
-							            .dependsOn(core)
+    lazy val scalajsSettings =
+      defaultSettings ++
+        scalaJSSettings ++
+        Seq(
+          libraryDependencies += "org.scala-lang.modules.scalajs" %% "scalajs-dom" % "0.2",
+          libraryDependencies += "org.scala-lang.modules.scalajs" %% "scalajs-jquery" % "0.2",
+          fork := true,
+          unmanagedSources in (Compile, packageJS) += baseDirectory.value / "js" / "startup.js")
+
+  }
+
+  lazy val root = Project(
+    id = "canrob-root",
+    base = file("."),
+    settings = S.defaultSettings)
+    .aggregate(core, swing, scalajs)
+    
+  lazy val core = Project(
+    id = "canrob-core",
+    base = file("core"),
+    settings = S.coreSettings)
+    
+  lazy val swing = Project(
+    id = "canrob-swing",
+    base = file("swing"),
+    settings = S.swingSettings)
+    .dependsOn(core)
+    
+  lazy val scalajs = Project(
+    id = "canrob-scalajs",
+    base = file("scalajs"),
+    settings = S.scalajsSettings)
+    .dependsOn(core)
 
 }
